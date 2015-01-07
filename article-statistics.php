f00a4a66d6392d69258ca771a2e389ac563b3967
Rastislav Lamos <lamos.rasto@gmail.com>
2015-01-07 13:01:00 +0100

    add initial plugin structure and meta

---
+++ article-statistics.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Plugin Name:       Article Statistics
+ * Plugin URI:        https://lamosty.com/plugins/article-statistics-wordpress-plugin/
+ * Description:       Count characters, words, syllables, sentences and much more in your posts or pages while writing them or in the admin bar while browsing your site.
+ * Version:           1.0
+ * Author:            Rastislav Lamos
+ * Author URI:        https://lamosty.com
+ * License: GPL2+
+ *
+ * Text Domain: article-statistics
+ * Domain Path: /languages/
+ *
+ * Requires at least: 4.1
+ * Tested up to:      4.1
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly
+}
